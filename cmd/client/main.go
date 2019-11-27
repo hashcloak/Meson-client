d2ac1f72d561840d2f860bc785968d33216aeaf6
Mikerah <mikerahqc@protonmail.com>
2019-11-26 21:27:04 -0500

    finished basic client. Effectively forked from the currency repo

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,48 +5,28 @@ import (
     "flag"
     "math/big"
 
-    "github.com/hashcloak/Meson-client"
-    "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/core/types"
-    "github.com/ethereum/go-ethereum/crypto"
     "github.com/katzenpost/client"
 	"github.com/katzenpost/client/config"
-	"github.com/katzenpost/currency/common"
+	"github.com/HashCloak/Meson/common"
 	"github.com/ugorji/go/codec"
 )
 
-type Transaction struct {
-    Nonce uint64
-    ToAddress string
-    Amount *big.Int
-    GasLimit uint64
-    GasPrice *big.Int
-    Data []byte
-}
-
-func createRawTransactionBlob(transaction *Transaction, chainId *big.Int, privKey *ecdsa.PrivateKey) (string, error){
-    tx := types.NewTransaction(nonce, common.HexToAddress(address), amount, gasLimit, gasPrice, data)
-    signedTx := types.SignTx(tx, types.NewEIP155Signer(chainId), privKey)
-    ts := types.Transactions{signedTx}
-    return fmt.Sprintf("%x", ts.getRlp(0)), nil
-}
-
 func main() {
-    toAddr := flag.String()
-    amount := flag.Int()
-    gasLimit := flag.Int()
-    gasPrice := flag.Int()
-
-    cfgFile := flag.Strin()
-    ticker := flag.String()
-    service := flag.String()
+    cfgFile := flag.String("c", "katzenpost.toml", "Path to the server config file")
+    ticker := flag.String("t", "", "Ticker")
+    chainID := flag.Int("chain", 1, "Chain ID for specific ETH-based chain")
+    service := flag.String("s", "", "Service Name")
+    rawTransactionBlob := flag.String("rt", "", "Raw Transaction blob to send over the network")
     flag.parse()
 
-    // TODO: Error check all the passed in commandline options
+    if *rawTransactionBlob == "" {
+        panic("must specify a transaction blob in hex")
+    }
 
-    // TODO: Get Private Key from keystore
-
-    // TODO: Create raw transaction blob
+    cfg, err = config.LoadFile(*cfgFile)
+    if err != nil {
+        panic(err)
+    }
 
     cfg, linkKey := client.AutoRegisterRandomClient(cfg)
 	c, err := client.New(cfg)
@@ -59,16 +39,16 @@ func main() {
 		panic(err)
 	}
 
-	// serialize our transaction inside a zcash kaetzpost request message
-	req := common.NewRequest(*ticker, *hexBlob)
-	currencyRequest := req.ToJson()
+	// serialize our transaction inside a eth kaetzpost request message
+	req := common.NewRequest(*ticker, *rawTransactionBlob, *chainID)
+	mesonRequest := req.ToJson()
 
-	currencyService, err := session.GetService(*service)
+	mesonService, err := session.GetService(*service)
 	if err != nil {
 		panic(err)
 	}
 
-	reply, err := session.BlockingSendUnreliableMessage(currencyService.Name, currencyService.Provider, currencyRequest)
+	reply, err := session.BlockingSendUnreliableMessage(mesonService.Name, mesonService.Provider, mesonRequest)
 	if err != nil {
 		panic(err)
 	}
