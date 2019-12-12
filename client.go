cf47e54c3c9276592839f042607be85911945310
Sebastian Bolaños <10123236+sebohe@users.noreply.github.com>
2019-12-12 14:05:04 -0600

    Fixed the tests to make them pass (#19)
    * Fixed the tests to make them pass
    
    The steps done to fix the tests are as following:
    
    The first thing was to make the embedding of the Client
    structs more clear by importing the katzenpost/client as katzenClient
    since it seems that in some occasions it was getting overwritten. The
    embedded katzenpost/client.Client was also initialized in the New function
    and the exterior Meson-client.Client was intialized following the patterns
    of the former.
    
    The second thing done to make the tests pass was to change the expected values.
    In the first test the client was not being started with the Start() function. In the second function the expect values got changed from gor to +gor and the reply had the trailling zeroes removed.
    
    * Update travis tests
    
    * Update path for travis and add new caches
    
    * Removed Dockerfile. Moved it to hashcloak/dlv
    
    * Removed debugging part of the README.md
    
    * Fixed spelling

--- client.go
+++ client.go
@@ -1,50 +1,43 @@
 package client
 
 import (
-	"sync"
-
+	"errors"
 	"github.com/hashcloak/Meson/plugin/pkg/common"
-	"github.com/katzenpost/client"
+	katzenClient "github.com/katzenpost/client"
 	"github.com/katzenpost/client/config"
 	"github.com/katzenpost/core/crypto/ecdh"
 	"github.com/katzenpost/core/log"
 	"gopkg.in/op/go-logging.v1"
+	"path/filepath"
+	"sync"
 )
 
 type Client struct {
-	*client.Client
+	*katzenClient.Client
+
 	cfg        *config.Config
 	logBackend *log.Backend
 	log        *logging.Logger
 	fatalErrCh chan error
 	haltedCh   chan interface{}
 	haltOnce   *sync.Once
-	session    *client.Session
+	session    *katzenClient.Session
 	linkKey    *ecdh.PrivateKey
 	service    string
 }
 
-func (c *Client) Start() {
+func (c *Client) Start() error {
+	var err error
 	// Retrieve PKI consensus documents and related info
-	cfg, linkKey := client.AutoRegisterRandomClient(c.cfg)
-	c.cfg = cfg
-	c.linkKey = linkKey
-	session, err := c.NewSession(c.linkKey)
-	if err != nil {
-		panic(err)
-	}
-	c.session = session
-}
-
-func (c *Client) Stop() {
-	// shutdown client and clean up any threads
-	c.Shutdown()
+	_, c.linkKey = katzenClient.AutoRegisterRandomClient(c.cfg)
+	c.session, err = c.NewSession(c.linkKey)
+	return err
 }
 
 func (c *Client) SendRawTransaction(rawTransactionBlob *string, chainID *int, ticker *string) ([]byte, error) {
 	// Send a raw transaction blob to a provider
 	// Pretty much the same code that's in main.go
-	defer c.Stop()
+	defer c.Shutdown()
 
 	req := common.NewRequest(*ticker, *rawTransactionBlob, *chainID)
 	mesonRequest := req.ToJson()
@@ -62,9 +55,36 @@ func (c *Client) SendRawTransaction(rawTransactionBlob *string, chainID *int, ti
 	return reply, nil
 }
 
+func (c *Client) InitLogging() error {
+	f := c.cfg.Logging.File
+	if !c.cfg.Logging.Disable && c.cfg.Logging.File != "" {
+		if !filepath.IsAbs(f) {
+			return errors.New("log file path must be absolute path")
+		}
+	}
+
+	var err error
+	c.logBackend, err = log.New(f, c.cfg.Logging.Level, c.cfg.Logging.Disable)
+	if err == nil {
+		c.log = c.logBackend.GetLogger("katzenpost/client")
+	}
+	return err
+}
+
 // Creates a new Meson Client with the provided configuration
-func New(cfg *config.Config, service string) (*Client, error) {
+func New(cfgFile string, service string) (*Client, error) {
+	cfg, err := config.LoadFile(cfgFile)
+	if err != nil {
+		return nil, err
+	}
+
+	katzen, err := katzenClient.New(cfg)
+	if err != nil {
+		return nil, err
+	}
+
 	client := &Client{
+		Client:     katzen,
 		cfg:        cfg,
 		fatalErrCh: make(chan error),
 		haltedCh:   make(chan interface{}),
@@ -72,6 +92,20 @@ func New(cfg *config.Config, service string) (*Client, error) {
 		linkKey:    new(ecdh.PrivateKey),
 		service:    service,
 	}
+
+	if err := client.InitLogging(); err != nil {
+		return nil, err
+	}
+
+	// Start the fatal error watcher.
+	go func() {
+		err, ok := <-client.fatalErrCh
+		if !ok {
+			return
+		}
+		client.log.Warningf("Shutting down due to error: %v", err)
+		client.Shutdown()
+	}()
 
 	return client, nil
 }
---
+++ client_test.go
@@ -0,0 +1,52 @@
+package client
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestClientConnectShutdown(t *testing.T) {
+	require := require.New(t)
+
+	client, err := New("testdata/client.toml", "gor")
+	require.NoError(err)
+
+	err = client.Start()
+	require.NoError(err)
+
+	<-client.session.EventSink
+
+	client.Shutdown()
+	client.Wait()
+
+}
+
+func TestClientSendTransaction(t *testing.T) {
+	require := require.New(t)
+
+	client, err := New("testdata/client.toml", "gor")
+	require.NoError(err)
+	require.NoError(client.Start())
+
+	description, err := client.session.GetService("gor")
+	require.NoError(err)
+	// Notice that the service returns a + sign
+	require.True(description.Name == "+gor")
+	require.True(description.Provider == client.cfg.Panda.Provider)
+
+	rtBlob := "0xf8640c843b9aca0083030d409400b1c66f34d680cb8bf82c64dcc1f39be5d6e77501802da03b274f8e63ce753e1ccdd03ac2d5e2595ef605335ed4962fe058eb667dbf9e6ba07c91420f9cb9805b18c6f25f84e530b35fca9eb45e4c3f6e6d624f53a3a76c40"
+	chainID := 5
+	ticker := "gor"
+	reply, err := client.SendRawTransaction(&rtBlob, &chainID, &ticker)
+	require.NoError(err)
+
+	expected := []byte(`{"Message":"success","StatusCode":0,"Version":0}`)
+	// The reply comes with zero padding on the right.
+	// We only care about the contents of the message for now therefore
+	// we trim the trailing zeroes by the length of the value we expect
+	trimmedReply := reply[:len(expected)]
+	require.EqualValues(expected, trimmedReply)
+
+	client.Shutdown()
+	client.Wait()
+}
