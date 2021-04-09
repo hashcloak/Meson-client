dccd396a6dfe9ed18ad380858751ffacf3625e99
greatect <greatect@users.noreply.github.com>
2021-04-09 17:31:42 +0800

    Implement pki client with katzenmint

--- go.mod
+++ go.mod
@@ -6,14 +6,16 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/ethereum/go-ethereum v1.9.10
 	github.com/hashcloak/Meson-plugin v0.0.0-20200208012317-7f72000d5e7b
+	github.com/hashcloak/katzenmint-pki v0.0.0-20210409083424-225d4838706c
 	github.com/katzenpost/authority v0.0.9
 	github.com/katzenpost/client v0.0.3
-	github.com/katzenpost/core v0.0.8
+	github.com/katzenpost/core v0.0.11
 	github.com/katzenpost/minclient v0.0.6
 	github.com/katzenpost/registration_client v0.0.1
-	github.com/stretchr/testify v1.4.0
-	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80
-	golang.org/x/text v0.3.2
+	github.com/stretchr/testify v1.7.0
+	github.com/tendermint/tendermint v0.34.6
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
+	golang.org/x/text v0.3.4
 	gopkg.in/eapache/channels.v1 v1.1.0
 	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473
 )
---
+++ minclient/pkiclient.go
@@ -0,0 +1,165 @@
+// pkiclient.go - katzenmint implementation of katzenpost PKIClient interface
+
+package minclient
+
+import (
+	"context"
+	"crypto/ed25519"
+	"encoding/json"
+	"fmt"
+
+	kpki "github.com/hashcloak/katzenmint-pki"
+	"github.com/hashcloak/katzenmint-pki/s11n"
+	"github.com/katzenpost/core/crypto/eddsa"
+	"github.com/katzenpost/core/log"
+	cpki "github.com/katzenpost/core/pki"
+	"github.com/tendermint/tendermint/light"
+	"github.com/tendermint/tendermint/light/provider"
+	lightrpc "github.com/tendermint/tendermint/light/rpc"
+	"github.com/tendermint/tendermint/light/store"
+	"github.com/tendermint/tendermint/rpc/client/http"
+	"gopkg.in/op/go-logging.v1"
+)
+
+var _ cpki.Client = (*PKIClient)(nil)
+
+type PKIClientConfig struct {
+	LogBackend   *log.Backend
+	TrustOptions light.TrustOptions
+	Primary      provider.Provider
+	Witnesses    []provider.Provider
+	TrustedStore store.Store
+	Rpcaddress   string
+}
+
+type PKIClient struct {
+	light *lightrpc.Client
+	log   *logging.Logger
+}
+
+func (p *PKIClient) Get(ctx context.Context, epoch uint64) (*cpki.Document, []byte, error) {
+	p.log.Debugf("Get(ctx, %d)", epoch)
+
+	// Form the abci query
+	query, err := json.Marshal(kpki.Query{
+		Version: "1",
+		Epoch:   epoch,
+		Command: kpki.GetConsensus,
+		Payload: "",
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	p.log.Debugf("Query: %v", query)
+
+	// Make the abci query
+	resp, err := p.light.ABCIQuery(ctx, "TODO: /path", query)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Check for response status
+	if resp.Response.Code != 0 {
+		return nil, nil, cpki.ErrNoDocument
+	}
+
+	// Verify and parse the document
+	doc, err := s11n.VerifyAndParseDocument(resp.Response.Value, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if doc.Epoch != epoch {
+		p.log.Warningf("Get() returned pki document for wrong epoch: %v", doc.Epoch)
+		return nil, nil, s11n.ErrInvalidEpoch
+	}
+	p.log.Debugf("Document: %v", doc)
+
+	return doc, resp.Response.Value, nil
+}
+
+func (p *PKIClient) Post(ctx context.Context, epoch uint64, signingKey *eddsa.PrivateKey, d *cpki.MixDescriptor) error {
+	p.log.Debugf("Post(ctx, %d, %v, %+v)", epoch, signingKey.PublicKey(), d)
+
+	// Ensure that the descriptor we are about to post is well formed.
+	if err := s11n.IsDescriptorWellFormed(d, epoch); err != nil {
+		return err
+	}
+
+	// Make a serialized + signed + serialized descriptor.
+	signed, err := s11n.SignDescriptor(signingKey, d)
+	if err != nil {
+		return err
+	}
+	p.log.Debugf("Signed descriptor: '%v'", signed)
+
+	// Form the abci transaction
+	rawTx := kpki.Transaction{
+		Version: "1",
+		Epoch:   epoch,
+		Command: kpki.PublishMixDescriptor,
+		Payload: kpki.EncodeHex(signed),
+	}
+	// ! Problemsome here
+	rawTx.AppendSignature(ed25519.PrivateKey(signingKey.Bytes()))
+	tx, err := json.Marshal(rawTx)
+	if err != nil {
+		return err
+	}
+	p.log.Debugf("Transaction: '%v'", tx)
+
+	// Broadcast the abci transaction
+	resp, err := p.light.BroadcastTxSync(ctx, tx)
+	if err != nil {
+		return err
+	}
+	if resp.Code != 0 {
+		return fmt.Errorf("Broadcast Tx returned with status code: %v", resp.Code)
+	}
+	// TODO: Make sure to subscribe for events
+
+	// Parse the post_descriptor_status command.
+	/*
+		r, ok := resp.(*commands.PostDescriptorStatus)
+		if !ok {
+			return fmt.Errorf("nonvoting/client: Post() unexpected reply: %T", resp)
+		}
+		switch r.ErrorCode {
+		case commands.DescriptorOk:
+			return nil
+		case commands.DescriptorConflict:
+			return cpki.ErrInvalidPostEpoch
+		default:
+			return fmt.Errorf("nonvoting/client: Post() rejected by authority: %v", postErrorToString(r.ErrorCode))
+		}
+	*/
+	return nil
+}
+
+func (p *PKIClient) Deserialize(raw []byte) (*cpki.Document, error) {
+	return s11n.VerifyAndParseDocument(raw, nil)
+}
+
+func NewPKIClient(cfg *PKIClientConfig) (cpki.Client, error) {
+	p := new(PKIClient)
+	p.log = cfg.LogBackend.GetLogger("pki/client")
+
+	provider, err := http.New(cfg.Rpcaddress, "/websocket")
+	if err != nil {
+		p.log.Errorf("Error connection to katzenmint-pki full node.")
+		return nil, err
+	}
+	lightclient, err := light.NewClient(
+		context.TODO(),
+		"TODO: chainID_of_katzenmint_pki",
+		cfg.TrustOptions,
+		cfg.Primary,
+		cfg.Witnesses,
+		cfg.TrustedStore,
+	)
+	if err != nil {
+		p.log.Errorf("Error initialization of katzenmint-pki light client.")
+		return nil, err
+	}
+	p.light = lightrpc.NewClient(provider, lightclient)
+	return p, nil
+}
